d827a1c34d11bacb247dd6cd2f876af4c39b16af
Luis Fernando Gaido <lfgaido@correios.com.br>
2018-11-16 09:20:43 -0200

    Tudo que eu queria implementado.

--- README.md
+++ README.md
@@ -5,8 +5,27 @@ Calcula a diferença entre horários e durações.
 ##Uso
 
 Calcula a duração entre dois horários:
-
 ```
 ~$ tdiff 15:45 13:31
 ```
+
+Calcula a duração entre duas datas/horas:
+```
+~$ tdiff 2018-11-16T15:45 2018-11-15T13:31
+```
+
+Calcula a diferença entre um horário e uma duração:
+
+```
+~$ tdiff 15:45 43m
+```
+
+O horário inicial pode ser omitido se referir à hora atual:
+```
+~$ tdiff 2h43
+``` 
+ou:
+```
+~$ tdiff 13:31
+``` 
 
--- main.go
+++ main.go
@@ -1,10 +1,79 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
 	"os"
+	"strings"
+	"time"
 )
 
 func main() {
-	fmt.Println(os.Args[:])
+	ret, err := calcula(os.Args[1:]...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(ret)
+}
+
+func calcula(pars ...string) (string, error) {
+	var p1, p2 string
+	switch len(pars) {
+	case 1:
+		p1 = time.Now().Format("15:04")
+		p2 = pars[0]
+	case 2:
+		p1 = pars[0]
+		p2 = pars[1]
+	default:
+		return "", errors.New("um ou dois parâmetros esperados")
+	}
+	if strings.Contains(p2, "h") || strings.Contains(p2, "m") {
+		t, err := calcTim(p1, p2)
+		return t.Format("2006-01-02 15:04"), err
+	}
+	dur, err := calcDur(p1, p2)
+	return dur.String(), err
+}
+
+func calcDur(p1, p2 string) (time.Duration, error) {
+	ini, err := analisaDataHora(p1)
+	if err != nil {
+		return 0, err
+	}
+	fim, err := analisaDataHora(p2)
+	if err != nil {
+		return 0, err
+	}
+	return fim.Sub(ini), nil
+}
+
+func calcTim(p1, p2 string) (time.Time, error) {
+	t, err := analisaDataHora(p1)
+	if err != nil {
+		return time.Time{}, err
+	}
+	d, err := time.ParseDuration("-" + p2)
+	if err != nil {
+		return time.Time{}, err
+	}
+	return t.Add(d), nil
+
+}
+
+func analisaDataHora(s string) (time.Time, error) {
+	var (
+		t   time.Time
+		err error
+	)
+	switch len(s) {
+	case 5:
+		t, err = time.Parse("2006-01-02T15:04", time.Now().Format("2006-01-02")+"T"+s)
+	case 16:
+		t, err = time.Parse("2006-01-02T15:04", s)
+	default:
+		err = errors.New("data/hora inválida")
+	}
+	return t, err
 }
